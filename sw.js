729e55098145fca5b45c224971596128f481f63f
Ben Oakes <hello@benjaminoakes.com>
2020-06-04 07:12:27 -0500

    Add a little logging

--- sw.js
+++ sw.js
@@ -1,15 +1,19 @@
 self.addEventListener('install', function(event) {
+  console.log("install event")
+  console.log(event)
   event.waitUntil(
     caches.open('sw-cache').then(function(cache) {
-      return cache.add('index.html');
+      return cache.add('index.html')
     })
-  );
-});
+  )
+})
  
 self.addEventListener('fetch', function(event) {
+  console.log("fetch event")
+  console.log(event)
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
     })
-  );
-});
+  )
+})
