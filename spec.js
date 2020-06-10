0bd15fa3bb7c2500da5ce07be6a1c9f41058838d
Ben Oakes <hello@benjaminoakes.com>
2020-06-10 00:54:20 +0000

    Add unique

--- spec.js
+++ spec.js
@@ -5,11 +5,29 @@ mocha.setup("bdd")
 mocha.checkLeaks()
 
 describe("Commands", function () {
-  it("sortNumerically", function () {
+  it("sorts numerically", function () {
     let lines = ["8 h", "11 k", "2 b", "1 a", "21 v"]
     let expected = ["1 a", "2 b", "8 h", "11 k", "21 v"]
 
     let sorted = Commands.sortNumerically(lines)
+
+    expect(sorted).to.deep.equal(expected)
+  })
+
+  it("filters unique lines", function () {
+    let lines = ["a", "b", "c", "a", "b", "c", "d"]
+    let expected = ["a", "b", "c", "d"]
+
+    let sorted = Commands.unique(lines)
+
+    expect(sorted).to.deep.equal(expected)
+  })
+
+  it("keeps order of unique lines", function () {
+    let lines = ["d", "c", "b", "a", "c", "b", "a"]
+    let expected = ["d", "c", "b", "a"]
+    
+    let sorted = Commands.unique(lines)
 
     expect(sorted).to.deep.equal(expected)
   })
