4c81edd564e4fd900f83d30b4600644366732896
Ben Oakes <hello@benjaminoakes.com>
2020-06-20 06:50:00 -0500

    standardjs

--- spec.js
+++ spec.js
@@ -1,33 +1,36 @@
+/* eslint-env mocha */
+/* global chai, Commands */
+
 const expect = chai.expect
 
-mocha.setup("bdd")
+mocha.setup('bdd')
 // mocha.growl()
 mocha.checkLeaks()
 
-describe("Commands", function () {
-  it("sorts numerically", function () {
-    let lines = ["8 h", "11 k", "2 b", "1 a", "21 v"]
-    let expected = ["1 a", "2 b", "8 h", "11 k", "21 v"]
+describe('Commands', function () {
+  it('sorts numerically', function () {
+    const lines = ['8 h', '11 k', '2 b', '1 a', '21 v']
+    const expected = ['1 a', '2 b', '8 h', '11 k', '21 v']
 
-    let sorted = Commands.sortNumerically(lines)
-
-    expect(sorted).to.deep.equal(expected)
-  })
-
-  it("filters unique lines", function () {
-    let lines = ["a", "b", "c", "a", "b", "c", "d"]
-    let expected = ["a", "b", "c", "d"]
-
-    let sorted = Commands.unique(lines)
+    const sorted = Commands.sortNumerically(lines)
 
     expect(sorted).to.deep.equal(expected)
   })
 
-  it("keeps order of unique lines", function () {
-    let lines = ["d", "c", "b", "a", "c", "b", "a"]
-    let expected = ["d", "c", "b", "a"]
-    
-    let sorted = Commands.unique(lines)
+  it('filters unique lines', function () {
+    const lines = ['a', 'b', 'c', 'a', 'b', 'c', 'd']
+    const expected = ['a', 'b', 'c', 'd']
+
+    const sorted = Commands.unique(lines)
+
+    expect(sorted).to.deep.equal(expected)
+  })
+
+  it('keeps order of unique lines', function () {
+    const lines = ['d', 'c', 'b', 'a', 'c', 'b', 'a']
+    const expected = ['d', 'c', 'b', 'a']
+
+    const sorted = Commands.unique(lines)
 
     expect(sorted).to.deep.equal(expected)
   })
